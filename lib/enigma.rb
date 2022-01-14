f2641d046d6054b4e72afd6962ede8e5b7027f49
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-14 16:40:08 -0500

    feat: enigma can encrtyp and return a hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -19,7 +19,17 @@ class Enigma
     encrypted = message.shift
     final = {encryption: encrypted, key: key, date: date}
     final
-    binging.pry
+
+  end
+
+  def decrypt(message, key, date = (Date.today.strftime"%d%m%y").to_s)
+    key_set = Keymaker.new(key)
+    offset = Offsetmaker.new(date)
+    shiftmaker = Shiftmaker.new(key_set.makekeys, offset.makekeys)
+    message = LetterShift.new(message, shiftmaker.makekeys)
+    encrypted = message.shift(-1)
+    final = {encryption: encrypted, key: key, date: date}
+    final
   end
 
 end
