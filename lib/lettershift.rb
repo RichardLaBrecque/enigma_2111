d444695560861746d5b6173e173b35878231a4e2
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-14 15:57:07 -0500

    refactor: single method now handles encrypt/decrypt with optional argument

--- lib/lettershift.rb
+++ lib/lettershift.rb
@@ -6,16 +6,16 @@ class LetterShift
     @shift_hash = shift_hash
     @set = ("a".."z").to_a << " "
   end
-
-  def shift
+# optional 1 encrypts, (-1) decripts
+  def shift(direction = 1)
     keys = @shift_hash.keys
     rotator = @set
     encrypted_array = []
     @message_array.each do |letter|
       if @set.include?(letter)
-        original = @set.find_index(letter)
-        rotated = rotator.rotate(@shift_hash[keys[0]])
-        encrypted_array << rotated[original]
+        original_index = @set.find_index(letter)
+        rotated = rotator.rotate(direction * (@shift_hash[keys[0]]))
+        encrypted_array << rotated[original_index]
         keys.rotate!
       else
         encrypted_array << letter
@@ -24,4 +24,22 @@ class LetterShift
     encrypted_array.join
     #binding.pry
   end
+  #no longer required, covered by optional argument for shift, (-1) reverses encrypt
+  # def unshift
+  #   keys = @shift_hash.keys
+  #   rotator = @set
+  #   encrypted_array = []
+  #   @message_array.each do |letter|
+  #     if @set.include?(letter)
+  #       #Sbinding.pry
+  #       original = @set.find_index(letter)
+  #       rotated = rotator.rotate(-(@shift_hash[keys[0]]))
+  #       encrypted_array << rotated[original]
+  #       keys.rotate!
+  #     else
+  #       encrypted_array << letter
+  #     end
+  #   end
+  #   encrypted_array.join
+  # end
 end
