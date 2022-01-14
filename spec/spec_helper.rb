142d1c29944c71b3dce81072b78e7affa8f2ab74
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-14 11:22:44 -0500

    simpleCov setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+SimpleCov.command_name 'Enigma coverage check'
+require_relative 'keymaker_spec'
+require_relative 'offsetmaker_spec'
+require_relative 'shiftmaker_spec'
